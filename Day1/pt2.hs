5828846bd9cb212e27807b5c705499a925a18383
Ross LeBeau <ross.lebeau@gmail.com>
2015-12-02 20:51:58 -0500

    Safe solution to Day 1 Part 2

--- Day1/pt2.hs
+++ Day1/pt2.hs
@@ -1,13 +1,19 @@
 import System.IO 
+import Data.List
 
 valueForChar :: Char -> Int
 valueForChar '(' = 1
 valueForChar ')' = -1
 valueForChar _ = 0
 
+firstNegativeFloor :: [Char] -> Maybe Int
+firstNegativeFloor directions = case (elemIndex (-1) $ takeWhile (>=0) (scanl (+) 0 (map valueForChar directions))) of
+                                    Just i -> Just (i + 1)
+                                    Nothing -> Nothing
+
 main :: IO ()
 main = do
     inputHandle <- openFile "pt2_input" ReadMode
     input <- hGetContents inputHandle
 
-    print $ length $ takeWhile (>=0) (scanl (+) 0 (map valueForChar input))
+    print $ elemIndex (-1) $ (scanl (+) 0 (map valueForChar input))
