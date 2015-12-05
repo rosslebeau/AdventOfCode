e15228777ff7f32961e5545eec3bbc33782fc719
Ross LeBeau <ross.lebeau@gmail.com>
2015-12-04 21:06:38 -0500

    Clean up for Day 3 Part 2

--- Day3/pt2.hs
+++ Day3/pt2.hs
@@ -13,10 +13,13 @@ nextHouse (startX, startY) direction
     | direction == '>' = (startX + 1, startY)
     | direction == 'v' = (startX, startY - 1)
     | direction == '<' = (startX - 1, startY)
-    | otherwise = (0,0) -- It's specified that this won't happen
+    | otherwise = (0, 0) -- It's specified that this won't happen
 
 housesFromDirections :: [Char] -> [House]
-housesFromDirections directions = concat [(scanl nextHouse (0,0) (everyOther directions)), (scanl nextHouse (0,0) (everyOther (drop 1 directions)))]
+housesFromDirections directions = scanl nextHouse (0,0) directions
+
+housesFromAlternatingDirections :: [Char] -> [House]
+housesFromAlternatingDirections directions = concat [(housesFromDirections (everyOther directions)), (housesFromDirections (everyOther (drop 1 directions)))]
 
 main :: IO ()
 main = do
@@ -24,4 +27,3 @@ main = do
     input <- hGetContents inputHandle
 
     print $ length $ nub $ housesFromDirections (input)
-    --print $ everyOther [1, 2, 3, 4, 5]
