ad34cd6b59840d428856b394aad64357c7045482
Ross LeBeau <ross.lebeau@gmail.com>
2015-12-05 14:31:28 -0500

    Day 5 Part 2

--- Day5/day5.hs
+++ Day5/day5.hs
@@ -1,14 +1,16 @@
 import System.IO
 import Text.Regex.PCRE
 
-isNice :: String -> Bool
-isNice string = (string =~ "(a.*|e.*|i.*|o.*|u.*){3,}" :: Bool) && (string =~ "(.)\\1" :: Bool) && (not (string =~ "ab|cd|pq|xy"))
+isNice1 :: String -> Bool
+isNice1 string = (string =~ "(a.*|e.*|i.*|o.*|u.*){3,}" :: Bool) && (string =~ "(.)\\1" :: Bool) && (not (string =~ "ab|cd|pq|xy"))
+
+isNice2 :: String -> Bool
+isNice2 string = (string =~ "(..).*\\1" :: Bool) && (string =~ "(.).\\1" :: Bool)
 
 main :: IO ()
 main = do
     inputHandle <- openFile "input" ReadMode
     input <- hGetContents inputHandle
 
-    print $ length $ filter (== True) $ map isNice (words input)
-    
-    --print $ matchRegex (mkRegex "(.)\\1{2,}") "aa"
+    print $ ("Part 1: " ++) . show $ length $ filter (== True) $ map isNice1 (words input)
+    print $ ("Part 2: " ++) . show $ length $ filter (== True) $ map isNice2 (words input)
