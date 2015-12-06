a37bf88ac35b48db7e8f1381e66628bbb77826de
Ross LeBeau <ross.lebeau@gmail.com>
2015-12-06 17:15:50 -0500

    Cleanup and solution for Day 6 Part 2

--- Day6/day6.hs
+++ Day6/day6.hs
@@ -10,44 +10,75 @@ type Lights = HashMap.HashMap Coordinate Int
 rectCornersFromString :: String -> (Coordinate, Coordinate)
 rectCornersFromString string = case getAllTextSubmatches (string =~ "([0-9]+),([0-9]+) through ([0-9]+),([0-9]+)" :: AllTextSubmatches [] String) of
                                 [_, lx, ly, ux, uy] -> ((read lx :: Int, read ly :: Int), (read ux :: Int, read uy :: Int))
-                                otherwise -> error "no"
+                                otherwise -> error ("Coordinates not found in instruction: " ++ string)
 
 coordinatesFromRectCorners :: Coordinate -> Coordinate -> [Coordinate]
 coordinatesFromRectCorners (lx, ly) (ux, uy) = concat $ map ((zip [lx..ux]) . repeat) [ly..uy]
 
-initialHashMap :: Lights
-initialHashMap = turnOff HashMap.empty (coordinatesFromRectCorners (0, 0) (999, 999))
+initialLights :: Lights
+initialLights = turnOff1 HashMap.empty (coordinatesFromRectCorners (0, 0) (999, 999))
 
-turnOn :: Lights -> [Coordinate] -> Lights
-turnOn lights coords = foldl' (\lights coord -> HashMap.insert coord 1 lights) lights coords
+-- Part 1
 
-turnOff :: Lights -> [Coordinate] -> Lights
-turnOff lights coords = foldl' (\lights coord -> HashMap.insert coord 0 lights) lights coords
+turnOn1 :: Lights -> [Coordinate] -> Lights
+turnOn1 lights coords = foldl' (\lights coord -> HashMap.insert coord 1 lights) lights coords
 
-toggle :: Lights -> [Coordinate] -> Lights
-toggle lights coords = foldl' (\lights coord -> HashMap.insert coord (oppositeVal coord lights) lights) lights coords
+turnOff1 :: Lights -> [Coordinate] -> Lights
+turnOff1 lights coords = foldl' (\lights coord -> HashMap.insert coord 0 lights) lights coords
+
+toggle1 :: Lights -> [Coordinate] -> Lights
+toggle1 lights coords = foldl' (\lights coord -> HashMap.insert coord (oppositeVal coord lights) lights) lights coords
     where oppositeVal c l = case HashMap.lookup c l of Just x -> x `xor` 1
-                                                       Nothing -> error "Nope"
+                                                       Nothing -> error ("Coordinate out of bounds: " ++ (show c))
 
-performOperations :: Lights -> [String] -> Lights
-performOperations lights [] = lights
-performOperations lights (o:os) = performOperations (performOperation lights o) os
+performOperations1 :: Lights -> [String] -> Lights
+performOperations1 lights [] = lights
+performOperations1 lights (o:os) = performOperations1 (performOperation1 lights o) os
 
-performOperation :: Lights -> String -> Lights
-performOperation lights operation
-    | isPrefixOf "turn on" operation = let (upperLeft, lowerRight) = (rectCornersFromString operation)
-                                       in turnOn lights (coordinatesFromRectCorners upperLeft lowerRight)
-    | isPrefixOf "turn off" operation = let (upperLeft, lowerRight) = (rectCornersFromString operation)
-                                       in turnOff lights (coordinatesFromRectCorners upperLeft lowerRight)
-    | isPrefixOf "toggle" operation = let (upperLeft, lowerRight) = (rectCornersFromString operation)
-                                       in toggle lights (coordinatesFromRectCorners upperLeft lowerRight)
-    | otherwise = error "hi"
+performOperation1 :: Lights -> String -> Lights
+performOperation1 lights operation
+    | isPrefixOf "turn on" operation = turnOn1 lights (coordinatesFromRectCorners upperLeft lowerRight)
+    | isPrefixOf "turn off" operation = turnOff1 lights (coordinatesFromRectCorners upperLeft lowerRight)
+    | isPrefixOf "toggle" operation = toggle1 lights (coordinatesFromRectCorners upperLeft lowerRight)
+    | otherwise = error ("Unsupported operation in instruction: " ++ operation)
+        where (upperLeft, lowerRight) = (rectCornersFromString operation)
+
+-- Part 2
+
+turnOn2 :: Lights -> [Coordinate] -> Lights
+turnOn2 lights coords = foldl' (\lights coord -> HashMap.insert coord (newVal coord lights) lights) lights coords
+    where newVal c l = case HashMap.lookup c l of Just x -> x + 1
+                                                  Nothing -> error ("Coordinate out of bounds: " ++ (show c))
+
+turnOff2 :: Lights -> [Coordinate] -> Lights
+turnOff2 lights coords = foldl' (\lights coord -> HashMap.insert coord (newVal coord lights) lights) lights coords
+    where newVal c l = case HashMap.lookup c l of Just x -> max (x - 1) 0
+                                                  Nothing -> error ("Coordinate out of bounds: " ++ (show c))
+
+toggle2 :: Lights -> [Coordinate] -> Lights
+toggle2 lights coords = foldl' (\lights coord -> HashMap.insert coord (newVal coord lights) lights) lights coords
+    where newVal c l = case HashMap.lookup c l of Just x -> x + 2
+                                                  Nothing -> error ("Coordinate out of bounds: " ++ (show c))
+
+performOperations2 :: Lights -> [String] -> Lights
+performOperations2 lights [] = lights
+performOperations2 lights (o:os) = performOperations2 (performOperation2 lights o) os
+
+performOperation2 :: Lights -> String -> Lights
+performOperation2 lights operation
+    | isPrefixOf "turn on" operation = turnOn2 lights (coordinatesFromRectCorners upperLeft lowerRight)
+    | isPrefixOf "turn off" operation = turnOff2 lights (coordinatesFromRectCorners upperLeft lowerRight)
+    | isPrefixOf "toggle" operation = toggle2 lights (coordinatesFromRectCorners upperLeft lowerRight)
+    | otherwise = error ("Unsupported operation in instruction: " ++ operation)
+        where (upperLeft, lowerRight) = (rectCornersFromString operation)
 
 main :: IO ()
 main = do
     inputHandle <- openFile "input" ReadMode
     input <- hGetContents inputHandle
 
-    let lights = performOperations initialHashMap (lines input)
-    
-    print $ HashMap.foldl' (\acc light -> acc + light) 0 lights
+    let lights1 = performOperations1 initialLights (lines input)
+    print $ ("Part 1: " ++) . show $ HashMap.foldl' (\acc light -> acc + light) 0 lights1
+
+    let lights2 = performOperations2 initialLights (lines input)
+    print $ ("Part 2: " ++) . show $ HashMap.foldl' (\acc light -> acc + light) 0 lights2
