76604401c30e83b0e9f33baf32e1cba19e14b911
Ross LeBeau <ross.lebeau@gmail.com>
2017-05-16 10:03:58 -0700

    old cleanup

--- Day12/day12.playground/Contents.swift
+++ Day12/day12.playground/Contents.swift
@@ -30,6 +30,30 @@ func sumAllNumbers(json: AnyObject, acc: Int) throws -> Int {
     }
 }
 
+func sumAllNumbersIgnoringRed(json: AnyObject, acc: Int) throws -> Int {
+    if json.isKindOfClass(NSNumber) {
+        let num = json as! NSNumber
+        return acc + num.integerValue
+    }
+    else if json.isKindOfClass(NSDictionary) {
+        let dict = json as! NSDictionary
+        return try dict.allKeys.map({key -> Int in
+            guard let val = dict.objectForKey(key) else {
+                throw AdventError.JSONError
+            }
+            return try sumAllNumbers(val, acc: 0)
+        })
+        .reduce(acc, combine: +)
+    }
+    else if json.isKindOfClass(NSArray) {
+        let arr = json as! NSArray
+        return try arr.reduce(0, combine: {try sumAllNumbers($1, acc: 0) + $0}) + acc
+    }
+    else {
+        return 0
+    }
+}
+
 do {
     guard let filePath = NSBundle.mainBundle().pathForResource("input", ofType: nil) else {
         throw AdventError.InputPathFailed
